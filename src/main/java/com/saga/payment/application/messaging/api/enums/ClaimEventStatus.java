f53fc38708464a439d56c26efae01df39344ea06
marijal74 <lakic.marija98@gmail.com>
2024-08-04 13:16:39 +0200

    Remove redundant statuses

--- src/main/java/com/saga/payment/application/messaging/api/enums/ClaimEventStatus.java
+++ src/main/java/com/saga/payment/application/messaging/api/enums/ClaimEventStatus.java
@@ -2,8 +2,6 @@ package com.saga.payment.application.messaging.api.enums;
 
 public enum ClaimEventStatus {
     CREATED,
-    RETURNING_TO_WAREHOUSE,
-    RETURN_TO_SELLER,
     REFUNDED,
     IN_DELIVERY,
     DELIVERED,
--- src/main/java/com/saga/payment/domain/model/enums/ClaimStatus.java
+++ src/main/java/com/saga/payment/domain/model/enums/ClaimStatus.java
@@ -2,8 +2,6 @@ package com.saga.payment.domain.model.enums;
 
 public enum ClaimStatus {
     CREATED,
-    RETURNING_TO_WAREHOUSE,
-    RETURN_TO_SELLER,
     REFUNDED,
     IN_DELIVERY,
     DELIVERED,
